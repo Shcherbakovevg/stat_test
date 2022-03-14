503c1148b24db5dc568fd2fc05b15c7986c1f8df
Eugeniy <shcherbakoveugeniy@gmail.com>
2022-03-14 11:45:06 +0200

    add simple autotests

---
+++ src/index.py
@@ -0,0 +1,23 @@
+import random
+print ("Hello world!")
+
+def stat_1():
+    return (random.randint(1,10))
+def stat_2():
+    return (random.randint(1,10))
+def stat_3():
+    return (random.randint(1,10))
+def stat_4():
+    return (random.randint(1,10))
+def stat_5():
+    return (random.randint(1,10))
+def stat_6():
+    return (random.randint(1,10))
+def stat_7():
+    return (random.randint(1,10))
+def stat_8():
+    return (random.randint(1,10))
+def stat_9():
+    return (random.randint(1,10))
+def stat_10():
+    return (random.randint(1,10))
---
+++ test_stat.py
@@ -0,0 +1,33 @@
+import pytest
+from src.index import *
+
+def test_stat_1():
+    result = stat_1()
+    assert result % 2 == 0
+def test_stat_2():
+    result = stat_2()
+    assert result % 2 == 0
+def test_stat_3():
+    result = stat_3()
+    assert result % 2 == 0
+def test_stat_4():
+    result = stat_4()
+    assert result % 2 == 0
+def test_stat_5():
+    result = stat_5()
+    assert result % 2 == 0
+def test_stat_6():
+    result = stat_6()
+    assert result % 2 == 0
+def test_stat_7():
+    result = stat_7()
+    assert result % 2 == 0
+def test_stat_8():
+    result = stat_8()
+    assert result % 2 == 0
+def test_stat_9():
+    result = stat_9()
+    assert result % 2 == 0
+def test_stat_10():
+    result = stat_10()
+    assert result % 2 == 0
